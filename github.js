d5e1b06874d52044de2c97caff4664ff43139d88
Ann Allan <ann.cristina.allan@gmail.com>
2018-02-28 20:29:23 -0500

    Add fetch and display repos

--- github.js
+++ github.js
@@ -2,15 +2,23 @@ class Github {
   constructor() {
     this.client_id = 'b5d518aade0b02c3e4c2';
     this.client_secret = '4ccaf3ec2133eb2330ad512f3f4cd71159e70d8c';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
